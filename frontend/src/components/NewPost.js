6bd0cba1d81276a8f463811349b3c98af18c9808
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-04-05 21:26:45 -0300

    Adding the validation for create a new post.

--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { handleInitialCategories, handleInitialPosts} from '../actions/shared'
 import { handleAddPost } from '../actions/posts'
 import { Button, Col, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { AvRadioGroup, AvRadio, AvForm, AvField, AvGroup, AvFeedback  } from 'availity-reactstrap-validation';
 import { Link } from 'react-router-dom'
 
 class NewPost extends Component{
@@ -11,12 +12,7 @@ class NewPost extends Component{
         this.props.dispatch(handleInitialCategories())
     }
 
-    state = {
-        title : "",
-        body : "",
-        author : "",
-        categorie : ""
-    }
+    state = {}
 
     handleChangeBody = (e) => {
         const body = e.target.value
@@ -48,12 +44,9 @@ class NewPost extends Component{
         })
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-
+    handleSubmit = () => {
         const { title, body, author, categorie } = this.state;
         const { dispatch } = this.props
-
         dispatch(handleAddPost(title, body, author, categorie))
 
         this.setState(() => ({
@@ -64,92 +57,98 @@ class NewPost extends Component{
         }))
     }
 
+    validateButton = () => (
+        this.state.title && this.state.body && this.state.author && this.state.categorie
+    )
+
     render(){
 
         const { body } = this.state;
         {/* todo: Redirect to /if submitted */}
 
-        const charsLeft = 280 - body.length
+        //const charsLeft = 280 - body.length
 
         return (
-        <Form>
-            <FormGroup classname='new-post'>
-                <Col sm="12" md={{ size: 12, offset: 5 }}>
-                    <h3>Type a New Post</h3>
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Col sm="1" md={{ size: 1, offset: 2 }}>
-                    <p style={{textAlign:'end', verticalAlign:'baseline', paddingTop: "7px"}} for="titleID">Title:</p>
-                </Col>
-                <Col sm="6" md={{ size: 6, offset: 0}}>
-                    <Input
-                        type="title"
-                        name="title"
-                        id="titleID"
-                        placeholder="Type a title"
-                        onChange = {(e) => this.handleChangeTitle(e)}
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Col sm="1" md={{ size: 1, offset: 2 }}>
-                    <p style={{textAlign:'end', verticalAlign:'baseline', paddingTop: "7px"}} for="authorID">Author:</p>
-                </Col>
-                <Col sm="6" md={{ size: 6, offset: 0}}>
-                    <Input
-                        type="author"
-                        name="author"
-                        id="authorID"
-                        placeholder="Type your name"
-                        onChange = {(e) => this.handleChangeAuthor(e)}
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Col sm="1" md={{ size: 1, offset: 2 }}>
-                    <p style={{textAlign:'end', verticalAlign:'baseline', paddingTop: "12px"}} for="textID">Text Area:</p>
-                </Col>
-                <Col sm="6" md={{ size: 6, offset: 0}}>
-                    <Input
-                        type="textarea"
-                        name="text"
-                        id="textID"
-                        placeholder="Type your post"
-                        onChange = {(e) => this.handleChangeBody(e)}
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Col sm="2" md={{ size: 4, offset: 1 }}>
-                    <h5 style={{textAlign:'end', verticalAlign:'baseline', paddingRight: "12px"}}>Select the category:</h5>
-                </Col>
-                <Col sm="2" md={{ size: 3, offset: 0}}>
-                    {this.props.categories.map((categorie) => (
-                        <p style={{verticalAlign:'baseline', margin: "0px", paddingRight: "12px" }} check>
-                            <Input type="checkbox" onChange = {(e) => this.handleChangeCategorie(e)} value={categorie.name}/> {categorie.name}
-                        </p>
-                    ))}
-                    {/* <p style={{verticalAlign:'baseline', marginLeft: "-20px", paddingLeft: "-12px" }} check>
-                        <Input 
-                            type="text" 
-                            onChange = {(e) => this.handleChangeCategorie(e)} 
-                            placeholder="Type a new categorie" 
-                        /> 
-                    </p> */}
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Col sm="2" md={{ size: 3, offset: 5}}>
-                    <Button  color="primary" onClick={(e) => (this.handleSubmit(e))}>
-                        <Link to='/' exact activeClassName='active' style={{color : "black"}}>
-                            Submit
+        <div>
+            <AvForm onValidSubmit={this.handleSubmit}>
+                <AvGroup classname='new-post'>
+                    <Col sm="12" md={{ size: 12, offset: 5 }}>
+                        <h3>Type a New Post</h3>
+                    </Col>
+                </AvGroup>
+                <AvGroup row>
+                    <Col sm="1" md={{ size: 1, offset: 2 }}>
+                        <p style={{textAlign:'end', verticalAlign:'baseline', paddingTop: "7px"}} for="titleID">Title:</p>
+                    </Col>
+                    <Col sm="6" md={{ size: 6, offset: 0}}>
+                            <AvField 
+                                type="title"
+                                name="title"
+                                id="titleID"
+                                placeholder="Type a title"
+                                onChange = {(e) => this.handleChangeTitle(e)}
+                                required 
+                            />
+                    </Col>
+                </AvGroup>
+                <AvGroup row>
+                    <Col sm="1" md={{ size: 1, offset: 2 }}>
+                        <p style={{textAlign:'end', verticalAlign:'baseline', paddingTop: "7px"}} for="authorID">Author:</p>
+                    </Col>
+                    <Col sm="6" md={{ size: 6, offset: 0}}>
+                        <AvGroup>
+                            <AvField 
+                            type="author"
+                            name="author"
+                            id="authorID"
+                            placeholder="Type your name"
+                            onChange = {(e) => this.handleChangeAuthor(e)}
+                            required 
+                        />
+                        </AvGroup>
+                    </Col>
+                </AvGroup>
+                <AvGroup row>
+                    <Col sm="1" md={{ size: 1, offset: 2 }}>
+                        <p style={{textAlign:'end', verticalAlign:'baseline', paddingTop: "12px"}} for="textID">Text Area:</p>
+                    </Col>
+                    <Col sm="6" md={{ size: 6, offset: 0}}>
+                        <AvField 
+                            type="textarea"
+                            name="text"
+                            id="textID"
+                            placeholder="Type your post"
+                            onChange = {(e) => this.handleChangeBody(e)}
+                            required 
+                        />
+                    </Col>
+                </AvGroup>
+                <AvGroup row>
+                    <Col sm="2" md={{ size: 4, offset: 1 }}>
+                        <h5 style={{textAlign:'end', verticalAlign:'baseline', paddingRight: "12px"}}>Select the category:</h5>
+                    </Col>
+                    <Col sm="2" md={{ size: 3, offset: 0}}>
+                        <AvRadioGroup inline name="radioExample" required errorMessage="Pick one!">
+                            {this.props.categories.map((categorie) => (
+                                    <p style={{verticalAlign:'baseline', margin: "0px", paddingRight: "12px" }} check>
+                                        <AvRadio onChange = {(e) => this.handleChangeCategorie(e)} value={categorie.name}/> {categorie.name}
+                                    </p>
+                            ))}
+                        </AvRadioGroup>
+                    </Col>
+                </AvGroup>
+                <FormGroup row>
+                    <Col sm="2" md={{ size: 3, offset: 5}}>
+                        <Link to='/' exact activeClassName='active' style={{color : "black"}} onClick={e => this.validateButton()? this.handleSubmit() : e.preventDefault()}>
+                            <Button color= {this.validateButton()? 'primary' : "danger"} onclick={() => this.handleSubmit()}>
+                                    Submit
+                            </Button>
                         </Link>
-                    </Button>
-                </Col>
-            </FormGroup>
-        </Form>
-      
+                    </Col>
+                </FormGroup>
+            </AvForm>
+        </div>
+        
         )
     }
 }
