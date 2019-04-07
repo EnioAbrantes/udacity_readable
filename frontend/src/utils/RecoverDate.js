84dfdd3485df59c24d210dd80cdd873bff1d07eb
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-04-07 18:04:11 -0300

    Fixing the format of hour and minute.

--- frontend/src/utils/RecoverDate.js
+++ frontend/src/utils/RecoverDate.js
@@ -1,8 +1,11 @@
 export function recoverDate(timestamp) {
     let date = new Date(timestamp*1000)
+
+    let min = ("0" + date.getMinutes()).slice(-2)
+    let hour = ("0" + date.getHours()).slice(-2)
     let day = ("0" + date.getDate()).slice(-2)
     let month = ("0" + date.getMonth()).slice(-2)
     let year = ("0" + date.getFullYear()).slice(-2)
 
-    return `${date.getHours()}:${date.getMinutes()}   ${month}/${day}/${year}`
+    return `${hour}:${min}   ${month}/${day}/${year}`
 }
